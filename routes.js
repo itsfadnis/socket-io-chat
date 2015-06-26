3d65b737c31e16eb266e4f46c885b28b3dc8e0b3
itsfadnis <nik.fadnis@gmail.com>
2015-06-26 17:14:33 +0530

    Test commit

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 module.exports = function(app, participants) {
-	app.get('/', function(req, res) {
+	app.get('/', function(req, res) {	// index
 		res.sendfile(__dirname + '/index.html');
 	});
-	app.get('/participants', function(req, res) {
+	app.get('/participants', function(req, res) {	// returns participants on log in
 		res.send(participants);
 	});
 }
