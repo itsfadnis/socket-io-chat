21688696845056f02908ca36a53860756daddcc8
Nikhil Fadnis <nik.fadnis@gmail.com>
2015-06-27 17:51:19 +0530

    routes change

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 module.exports = function(app, participants) {
-	app.get('/', function(req, response) {
-		response.sendfile(__dirname + '/index.html');
+	app.get('/', function(req, res) {
+		res.sendfile(__dirname + '/index.html');
 	});
 	app.get('/participants', function(req, res) {
 		res.send(participants);
